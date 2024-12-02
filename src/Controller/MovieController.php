2c67c111420fc72210fa00ec8d7908d8200b8877
Robin Delbaere <hello@rdelbaere.fr>
2024-12-02 17:06:27 +0100

    Movies queries

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/movies', name: 'movie_')]
 class MovieController extends AbstractController
 {
+    public function __construct(private readonly MovieRepository $movieRepository)
+    {
+    }
+
     #[Route('', name: 'list', methods: ['GET'])]
     public function list(): Response
     {
+        $movies = $this->movieRepository->findBy([], ['releasedAt' => 'DESC']);
+        dd($movies);
+
         return new Response('Liste des films');
     }
 
-    #[Route('/{id}', name: 'single', requirements: ['id' => '\d+'])]
-    public function single(int $id): Response
+    #[Route('/{id:movie}', name: 'single', requirements: ['id' => '\d+'])]
+    public function single(Movie $movie): Response
     {
-        return new Response('Film : ' . $id);
+        return new Response('Film : ' . $movie->getTitle());
     }
 
     #[Route('/new', name: 'new')]
